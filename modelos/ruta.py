3be7dec8bde55be1607549b33532c931b66cc024
Angelina N <angelinanegrette25-dotcom>
2026-04-03 19:03:37 -0500

    Agregada clase Ruta con agregacion de Ubicaciones

--- modelos/ruta.py
+++ modelos/ruta.py
@@ -1,27 +1,10 @@
 
 class Ruta:
-    def _init_(self, id_ruta, origen, destino, distancia_km):
+    def __init__(self, id_ruta: int, nombre_ruta: str):
         self.id_ruta = id_ruta
-        self.origen = origen
-        self.destino = destino
-        self.distancia_km = distancia_km
-        self.puntos = []
-
+        self.nombre_ruta = nombre_ruta
+        self.puntos = [] # Lista de objetos Ubicacion
+    
     def agregar_punto(self, ubicacion):
         self.puntos.append(ubicacion)
-
-    def mostrar_ruta(self):
-        texto = (
-            f"Ruta {self.id_ruta}\n"
-            f"Origen: {self.origen}\n"
-            f"Destino: {self.destino}\n"
-            f"Distancia: {self.distancia_km} km\n"
-        )
-        if self.puntos:
-            texto += "Puntos de la ruta:\n"
-            for punto in self.puntos:
-                texto += f"- {punto}\n"
-        return texto
-
-    def _str_(self):
-        return f"Ruta {self.id_ruta}: {self.origen} -> {self.destino}"
+        
