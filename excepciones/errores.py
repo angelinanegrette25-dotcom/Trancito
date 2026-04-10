ad24393ac9a12e09c9c36a3a4ad78e7c2054c90e
Nicolle Posso Peña <nicolle@MacBook-Air-de-Nicolle.local>
2026-04-10 14:00:00 -0500

    Agregamos excepciones

---
+++ excepciones/errores.py
@@ -0,0 +1,14 @@
+class Usuario_No_encontrado(Exception):
+    pass
+
+
+class Vehiculo_No_encontrado(Exception):
+    pass
+
+
+class Zona_No_encontrada(Exception):
+    pass
+
+
+class Ruta_No_encontrada(Exception):
+    pass
